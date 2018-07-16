42aa5829f2d8f673bf2bffac3355ac2beafbe4a2
Nhut Nguyen <nguyencaominhnhut@gmail.com>
2018-07-16 21:00:49 +0700

    [FE]: Add register modal

--- src/main/resources/static/js/app/app.js
+++ src/main/resources/static/js/app/app.js
@@ -1,23 +1,19 @@
-var cafedevApp = angular.module('cafedevApp', [
-  'ngRoute',
-]);
+var cafedevApp = angular.module('cafedevApp', [ 'ngRoute', ]);
 
-cafedevApp.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/', {
-        templateUrl: 'views/home.html',
-        controller: 'HomeCtrl'
-      }).
-      when('/forum', {
-        templateUrl: 'views/forum.html',
-        controller: 'ForumCtrl'
-      }).
-      when('/topic', {
-          templateUrl: 'views/topic.html',
-          controller: 'TopicCtrl'
-        }).
-      otherwise({
-        redirectTo: '/'
-      });
-  }]);
+cafedevApp.config([ '$routeProvider', function($routeProvider) {
+	$routeProvider.when('/', {
+		templateUrl : 'views/home.html',
+		controller : 'HomeCtrl'
+	}).when('/forum', {
+		templateUrl : 'views/forum.html',
+		controller : 'ForumCtrl'
+	}).when('/topic/:id', {
+		templateUrl : 'views/topic.html',
+		controller : 'TopicCtrl'
+	}).when('/sub-topic/:id', {
+		templateUrl : 'views/sub-topic.html',
+		controller : 'SubTopicCtrl'
+	}).otherwise({
+		redirectTo : '/'
+	});
+} ]);
---
+++ src/main/resources/static/js/app/controllers/subtopic-controller.js
@@ -0,0 +1,3 @@
+cafedevApp.controller('SubTopicCtrl', ['$scope', function($scope){
+	$scope.message="Sub Topic Page"; 
+}]);
